25f08e95e399ba555dd2817f1f80a5670ad866b0
Fit-in-Linec <lukajelcic23@gmail.com>
2020-08-11 16:16:55 +0200

    add server side

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAcuiCLuYyYg2NvcFPhzINJfPkwggqntWc",
+    authDomain: "be-creative-dd183.firebaseapp.com",
+    databaseURL: "https://be-creative-dd183.firebaseio.com",
+    projectId: "be-creative-dd183",
+    storageBucket: "be-creative-dd183.appspot.com",
+    messagingSenderId: "552202256000",
+    appId: "1:552202256000:web:4dfe6966e0887c4587f065"
+};
